a6cafea4c4e5d32971b11242e29282531dc187df
m0000g <m0000g@gmail.com>
2022-10-13 15:04:25 -0600

    checking javascript DOM selectors

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log(`Test`);
+
+const elementbyID = document.getElementById("elementbyid");
+elementbyID.textContent = `Gotcha!`;
+
+const querySelector = document.querySelector(".querySelector");
+querySelector.textContent = `Also Gotcha!`;
+
+const getElementByClassName = document.getElementsByClassName("class_name");
+getElementByClassName[0].textContent = `Re-Gotcha!`;
