e8b8da07e6d987c2e804559d50f00fde95230df9
m0000g <m0000g@gmail.com>
2022-10-19 12:53:35 -0600

    Capitalize text JS function

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ querySelector.textContent = `Also Gotcha!`;
 
 const getElementByClassName = document.getElementsByClassName("class_name");
 getElementByClassName[0].textContent = `Re-Gotcha!`;
+
+const capitalize = function (myString) {
+  //const string = myString[0].toUpperCase();
+  //return string;
+  return myString.charAt(0).toUpperCase() + myString.substring(1);
+};
+console.log(
+  capitalize(`retornar el texto con la primera palabra en mayúscula.`)
+);
